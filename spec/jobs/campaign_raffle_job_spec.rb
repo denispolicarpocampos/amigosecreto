9c5757cedf77da01b2757af071ff1d954ad80a13
Denis Policarpo <campos_bra@hotmail.com>
2017-11-20 22:18:34 -0200

    Campaign raffle job test

--- spec/jobs/campaign_raffle_job_spec.rb
+++ spec/jobs/campaign_raffle_job_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe CampaignRaffleJob, type: :job do
-  pending "add some examples to (or delete) #{__FILE__}"
+  include ActiveJob::TestHelper
+  
+  before(:each) do
+    @current_user = FactoryGirl.create(:user)
+    @campaign = create(:campaign, user: @current_user)
+    @job = CampaignRaffleJob.perform_later(@campaign)
+  end
+
+  describe "#perform_later" do
+    it "job was enqueued" do
+      expect {CampaignRaffleJob.perform_later(@campaign)}.to have_enqueued_job
+    end
+
+    it "matches with enqueued job" do
+      expect {CampaignRaffleJob.perform_later(@campaign)}.to have_enqueued_job.on_queue("emails")
+    end
+  end
+
 end
